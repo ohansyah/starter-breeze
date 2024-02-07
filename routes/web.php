25b1143df14bf8d4b8b13cc18424befb09ca2d73
ohansyah <ohan.ohansyah@gmail.com>
2024-02-07 14:46:49 +0700

    INIT

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\Student\TimetableController;
+use App\Http\Controllers\Teacher\TimetableController as TeacherTimetable;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+ */
+
+Route::view('/', 'welcome');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+
+    Route::prefix('student')
+        ->name('student.')
+        ->group(function () {
+            Route::get('timetable', [TimetableController::class, 'index'])->name('timetable');
+        });
+
+    Route::prefix('teacher')
+        ->name('teacher.')
+        ->group(function () {
+            Route::get('timetable', [TeacherTimetable::class, 'index'])->name('timetable');
+        });
+
+        
+});
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__ . '/auth.php';
